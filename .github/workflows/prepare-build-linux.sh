a062f3676ed7dfd8ddf7fef9d251fed277cb0ba7
tashfernandes <tashfernandes@gmail.com>
2025-08-31 18:45:58 +1000

    Update prepare-build-linux.sh

--- .github/workflows/prepare-build-linux.sh
+++ .github/workflows/prepare-build-linux.sh
@@ -27,8 +27,9 @@ cd build
 cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
       -DPython3_EXECUTABLE=$PYTHON \
       -DPython_FIND_STRATEGY=LOCATION \
-      -DPython3_INCLUDE_DIR=$PYTHON_INCLUDE_DIR \
       -DPython3_ROOT_DIR=/opt/python/cp312-cp312 \
+      -DPython3_INCLUDE_DIRS=$PYTHON_INCLUDE_DIR \
+      -DPython3_LIBRARY=/opt/python/cp312-cp312/lib/libpython3.12.so \
       -DPython3_FIND_DEBUG=ON \
       $EXTRA_PY_ARGS ..
 #make install -j 2
